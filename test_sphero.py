686534229253d186f75f9507da130c67aba75379
peerdavid <d.peer@uibk.ac.at>
2020-01-18 22:04:40 +0100

    Fixed wrong use of reset_locator_x_and_y

--- test_sphero.py
+++ test_sphero.py
@@ -25,6 +25,9 @@ rvr = SpheroRvrAsync(
     )
 )
 
+location = {"x": 0.0, "y": 0.0}
+
+
 #
 # Sensor handlers
 #
@@ -32,9 +35,9 @@ async def locator_handler(locator_data):
     """ Handle location data (x,y) position of robot.
         See also https://community.sphero.com/t/programming-questions/829/4
     """
-    x = locator_data['Locator']['X']
-    y = locator_data['Locator']['Y']
-    print("Position: x: %.2f [m] | y: %.2f [m]" % (x, y))
+    location["x"] = locator_data['Locator']['X']
+    location["y"] = locator_data['Locator']['Y']
+    print("Position: x: %.2f [m] | y: %.2f [m]" % (location["x"], location["y"]))
 
 
 async def quaternion_handler(quaternion_data):
@@ -63,8 +66,11 @@ async def velocity_handler(velocity_data):
 #
 # M A I N
 #
-async def main(speed=45):
+async def main(speed=60, distance=2.5):
     """ This program has RVR drive around in different directions using the function drive_with_heading.
+
+    speed: Drive distance with given speed
+    distance: Distance to drive [m]
     """
     await rvr.wake()
     await asyncio.sleep(2)
@@ -73,6 +79,7 @@ async def main(speed=45):
     print("RVR data")
     print("------------------------------")
     print("Battery [%%]: %d" % (await rvr.get_battery_percentage())["percentage"])
+    print("MAC-Address: %s" % (await rvr.get_mac_address())["macAddress"])
     print("------------------------------")
 
     # Initialize sensors 
@@ -92,33 +99,35 @@ async def main(speed=45):
     #     service=RvrStreamingServices.velocity,
     #     handler=velocity_handler,
     # )
+    await rvr.sensor_control.start(interval=250)
 
-    await rvr.sensor_control.start(interval=100)
 
-    #
-    # Start driving
-    #
+    # Reset coordinates
     await rvr.reset_locator_x_and_y()
-    #await rvr.set_locator_flags(flags=1)
-    await rvr.reset_yaw()
-    await rvr.drive_with_heading(
-        speed=speed,    # Valid speed values are 0-255
-        heading=0,      # Valid heading values are 0-359
-        flags=DriveFlagsBitmask.none.value
-    )
-
-    
-    # Delay to allow RVR to drive
     await asyncio.sleep(2)
+
+    # Start driving
+    await rvr.reset_yaw()
+
+    # Delay to allow RVR to drive
+    while(location["y"] < distance):
+        await rvr.drive_with_heading(
+            speed=speed,    # Valid speed values are 0-255
+            heading=0,      # Valid heading values are 0-359
+            flags=DriveFlagsBitmask.none.value
+        )
+
+        await asyncio.sleep(0.25)
     
-    # We don't use roll_stop as this function is buggy. See also 
+    
+    # We don't use roll_stop as this function is currently buggy.
     await rvr.drive_with_heading(
-        speed=0,        # Valid speed values are 0-255
-        heading=180,      # Valid heading values are 0-359
+        speed=0,            # Valid speed values are 0-255
+        heading=180,        # Valid heading values are 0-359
         flags=DriveFlagsBitmask.none.value
     )
 
-    await asyncio.sleep(0.5)
+    await asyncio.sleep(2)
 
     # Delay to allow RVR to drive
 
