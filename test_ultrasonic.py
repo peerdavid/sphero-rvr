dd96e0874345c1ce40bea61dee95a7b70898e267
peerdavid <d.peer@uibk.ac.at>
2020-01-25 14:45:22 +0100

    Added left and right sensor to avoid stairs

--- test_ultrasonic.py
+++ test_ultrasonic.py
@@ -13,12 +13,25 @@ import asyncio
 #
 # GPIO Setup
 #
-trigger_pin = 23
-echo_pin = 24
+front_trigger_pin = 4
+front_echo_pin = 27
+
+right_trigger_pin = 18
+right_echo_pin = 23
+
+left_trigger_pin = 17
+left_echo_pin = 22
+
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(trigger_pin, GPIO.OUT)
-GPIO.setup(echo_pin, GPIO.IN)
+GPIO.setup(front_trigger_pin, GPIO.OUT)
+GPIO.setup(front_echo_pin, GPIO.IN)
+
+GPIO.setup(right_trigger_pin, GPIO.OUT)
+GPIO.setup(right_echo_pin, GPIO.IN)
+
+GPIO.setup(left_trigger_pin, GPIO.OUT)
+GPIO.setup(left_echo_pin, GPIO.IN)
 
 
 def measure_ultrasonic_distance(trigger, echo):
@@ -48,9 +61,18 @@ def measure_ultrasonic_distance(trigger, echo):
 
 def main():
     while True:
-        distance = measure_ultrasonic_distance(trigger_pin, echo_pin)
-        print(distance)
-        time.sleep(1)
+        start_time = time.time()
+        front_d = measure_ultrasonic_distance(front_trigger_pin, front_echo_pin)
+        right_d = measure_ultrasonic_distance(right_trigger_pin, right_echo_pin)
+        left_d = measure_ultrasonic_distance(left_trigger_pin, left_echo_pin)
+        stop_time = time.time()
+
+        time_elapsed = stop_time - start_time
+        print("Front: %.2f | Right: %.2f | Left: %.2f | Time elapsed: %.3f" % (front_d, right_d, left_d, time_elapsed), 
+            end="\r", 
+            flush=True)
+
+        time.sleep(0.1)
 
 if __name__ == '__main__':
     main()
