fa7a7a8853c53be02b18a7993e98b50cc07e8e35
peerdavid <d.peer@uibk.ac.at>
2020-02-02 18:30:37 +0100

    Security layer and tflite

--- tests/security_layer.py
+++ tests/security_layer.py
@@ -1,6 +1,6 @@
 #####################
-# Combine ultrasonic and rvr drive but avoid collisions and drops.
-#
+# Combine ultrasonic and rvr drive but avoid collisions and drops
+# and say hello to persons if detected...
 #  References:
 #  [1] Asyncio - https://docs.python.org/3/library/asyncio-task.html
 #####################
@@ -8,6 +8,7 @@
 import numpy as np
 import math
 import time
+import io
 
 import asyncio
 import concurrent.futures
@@ -22,6 +23,9 @@ from sphero_sdk import RvrLedGroups
 import RPi.GPIO as GPIO
 GPIO.setwarnings(False)
 
+from PIL import Image
+from picamera import PiCamera
+from tflite_runtime.interpreter import Interpreter
 
 
 class UltrasonicTimeout(Exception):
@@ -71,6 +75,10 @@ DRIVE_SLOW = 35
 DRIVE_NORMAL = 50
 DRIVE_FAST = 65
 
+# 
+CAMERA_WIDTH = 640
+CAMERA_HEIGHT = 480
+
 
 #
 # Init sphero rvr async sdk
@@ -102,34 +110,101 @@ tone.start(0)
 # Set State of rvr
 class RvrState: pass
 state = RvrState() 
-state.speed = 0
+state.watch_dog = time.monotonic()
 state.num_oks = 0
+state.persons = 0
+state.speed=0
 
 
 #
 # MACHINE LEARNING
 #
-def model():
-    x = 0
-    print("#### START INFERENCE")
-    for _ in range(10000):
-        x = 2 * 3 + x
-    return x
+def set_input_tensor(interpreter, image):
+  """Sets the input tensor."""
+  tensor_index = interpreter.get_input_details()[0]['index']
+  input_tensor = interpreter.tensor(tensor_index)()[0]
+  input_tensor[:, :] = image
 
 
-async def tf_inference():
-    """ Inference ONCE in a seperate process to not block anything else.
-    Note that this computation will be finsihed also if the main process is killed.
-    """
+def get_output_tensor(interpreter, index):
+  """Returns the output tensor at the given index."""
+  output_details = interpreter.get_output_details()[index]
+  tensor = np.squeeze(interpreter.get_tensor(output_details['index']))
+  return tensor
+
+
+def detect_objects(interpreter, image, threshold):
+  """Returns a list of detection results, each a dictionary of object info."""
+  set_input_tensor(interpreter, image)
+  interpreter.invoke()
+
+  # Get all output details
+  boxes = get_output_tensor(interpreter, 0)
+  classes = get_output_tensor(interpreter, 1)
+  scores = get_output_tensor(interpreter, 2)
+  count = int(get_output_tensor(interpreter, 3))
+
+  results = []
+  for i in range(count):
+    if scores[i] >= threshold:
+      result = {
+          'bounding_box': boxes[i],
+          'class_id': classes[i],
+          'score': scores[i]
+      }
+      results.append(result)
+  return results
+
+
+def inference():
+    global state
     
+    interpreter = Interpreter("models/mobilenet_v1/detect.tflite")
+    interpreter.allocate_tensors()
+    _, input_height, input_width, _ = interpreter.get_input_details()[0]['shape']
+    stream = io.BytesIO()
+
+    camera = PiCamera(resolution=(CAMERA_WIDTH, CAMERA_HEIGHT))
+    camera.start_preview()
+    camera.rotation=90
+
+    try:
+        while(True):
+
+            # last_ack_ms = (time.monotonic() - state.watch_dog) * 1000
+            # print("last_ack_ms", last_ack_ms)
+            # if last_ack_ms > 5000:
+            #     print("(Error) Main process died after %.3f." % last_ack_ms)
+            #     raise Exception("Main process died.")
+
+            print("Camera")
+            # Capture camera
+            camera.capture(stream, format='jpeg')
+            stream.seek(0)  # "Rewind" the stream to the beginning so we can read its content
+
+            # Get image from camera
+            image = Image.open(stream).convert('RGB').resize(
+                    (input_width, input_height), Image.ANTIALIAS) #.rotate(270)
+            
+            #image.save("capture.png")
+
+            # Inference given tflite model
+            print("Inference")
+            results = detect_objects(interpreter, image, 0.6)
+            state.persons = len([p for p in results if p["class_id"] == 0])
+            
+            # Clear camera stream
+            stream.seek(0)
+            stream.truncate()
+
+            time.sleep(2)
+    finally:
+        camera.stop_preview()
 
 
-async def cyclic_ml_check():
-    while True:
-        with concurrent.futures.ProcessPoolExecutor() as executor:
-            x = await loop.run_in_executor(executor, model)
-        print("#### STOP INFERENCE %d" % x)
-        asyncio.sleep(2)
+async def computer_vision():
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        await loop.run_in_executor(executor, inference)
 
 #
 # FUNCTIONS
@@ -218,43 +293,49 @@ async def say_hello():
 
 
 async def say_bye():
-    await play_tune(tone, 100, 8000)
-    await play_tune(tone, 100, 8000)
-    await play_tune(tone, 100, 8000)
-    await play_tune(tone, 100, 14000)
-    await play_tune(tone, 100, 8000)
+    await play_tune(tone, 200, 8000)
+    await play_tune(tone, 200, 4000)
 
 
 async def user_communication():
     global state
-    old_speed = state.speed
+    old_state = RvrState()
+    old_state.persons = 0
+    old_state.speed = 0
 
     while True:
         await asyncio.sleep(0.5)
         
-        if old_speed == state.speed:
-            continue
+        if old_state.speed != state.speed:
+            old_state.speed = state.speed
 
-        if state.speed == STOP:
-            await led_red()
-            await play_tune(tone, 100, 14000)
-            await play_tune(tone, 100, 8000)
-            await play_tune(tone, 400, 4000)
+            if state.speed == STOP:
+                await led_red()
+                #await play_tune(tone, 100, 14000)
+                #await play_tune(tone, 100, 8000)
+                #await play_tune(tone, 400, 4000)
 
-        elif state.speed == DRIVE_FAST:
-            await led_blue()
-            await play_tune(tone, 1000, 14000)
+            elif state.speed == DRIVE_FAST:
+                await led_blue()
+                #await play_tune(tone, 1000, 14000)
 
-        elif state.speed == DRIVE_NORMAL:
-            await led_green()
+            elif state.speed == DRIVE_NORMAL:
+                await led_green()
 
-        elif state.speed == DRIVE_SLOW:
-            await led_yellow()
+            elif state.speed == DRIVE_SLOW:
+                await led_yellow()
         
-        old_speed = state.speed
+        if old_state.persons != state.persons:
+            old_state.persons = state.persons
 
+            if state.persons <= 0:
+                await say_bye()
+            else:
+                await say_hello()
 
 async def stop_robot():
+    global state
+
     state.speed = STOP
     await rvr.raw_motors(0,0,0,0)
 
@@ -265,6 +346,9 @@ async def security_loop():
     num_oks = -50   # Init time
     ok_steps = 70
     while True:
+        # Update watchdog
+        state.watch_dog = time.monotonic()
+
         # Measure front
         try:
             await asyncio.sleep(0.005)
@@ -348,14 +432,14 @@ async def drive():
     ########################################
     while(True):
         await asyncio.sleep(0.01)
-        if state.speed <= 0:
-            continue
+        # if state.speed <= 0:
+        #     continue
 
-        await rvr.drive_with_heading(
-                speed=state.speed,
-                heading=0,
-                flags=DriveFlagsBitmask.none.value
-            )
+        # await rvr.drive_with_heading(
+        #         speed=state.speed,
+        #         heading=0,
+        #         flags=DriveFlagsBitmask.none.value
+        #     )
     ########################################
 
 
@@ -374,10 +458,9 @@ if __name__ == '__main__':
 
         loop.run_until_complete(
             asyncio.gather(
-                #cyclic_ml_check(),
-                say_hello(),
                 security_loop(),
                 drive(),
+                computer_vision(),
                 user_communication()
             )
         )
@@ -395,4 +478,3 @@ if __name__ == '__main__':
     finally:
         if loop.is_running():
             loop.close()
-            
